79dae37b2531d4edde17c16ccb0d5982c9856c51
Kenny Ferguson <121765690+Kennyfergy@users.noreply.github.com>
2023-10-04 18:24:30 -0500

    changed css

--- src/components/PlantDetails/PlantDetails.jsx
+++ src/components/PlantDetails/PlantDetails.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function PlantDetails() {
   const { id } = useParams();
-
+  const details = useSelector((store) => store.plant.plantsReducer);
+  console.log(details);
   // Fetch or retrieve more detailed data about the plant using the ID from the URL parameter
 
   return (
     <div>
       <h3>Details about plant: {id}</h3>
       {/* Render detailed information about the plant here */}
-      {/* {plantItem.kingdom} */}
+      <div>
+        {details.map((plantItem, id) => {
+          <li key={id}>{plantItem.name}</li>;
+        })}
+      </div>
     </div>
   );
 }
--- src/components/PlantItem/PlantItem.jsx
+++ src/components/PlantItem/PlantItem.jsx
@@ -1,45 +1,61 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
-import { useHistory } from "react-router-dom";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#4CAF50", // A green color
+    },
+    secondary: {
+      main: "#795548", // A brown color
+    },
+  },
+});
 
 function PlantItem({ plantItem }) {
   const dispatch = useDispatch();
   const history = useHistory();
 
   const handleSeeMoreClick = () => {
-    // Navigate to the plant details page
     history.push(`/plant/${plantItem.id}`);
   };
 
   return (
-    <List>
-      <ListItem>
-        <ListItemText primary={plantItem.name} />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSeeMoreClick}
-          style={{ marginRight: "8px" }}
-        >
-          See More Details
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() =>
-            dispatch({ type: "DELETE_PLANT", payload: plantItem.id })
-          }
-        >
-          Remove
-        </Button>
-      </ListItem>
-      <Divider />
-    </List>
+    <ThemeProvider theme={theme}>
+      <List>
+        <ListItem>
+          <ListItemText
+            primary={plantItem.name}
+            secondary={plantItem.kingdom}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSeeMoreClick}
+            style={{ marginRight: "8px" }}
+          >
+            See More Details
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() =>
+              dispatch({ type: "DELETE_PLANT", payload: plantItem.id })
+            }
+          >
+            Remove
+          </Button>
+        </ListItem>
+        <Divider />
+      </List>
+    </ThemeProvider>
   );
 }
 
